c5568c379ab1c8e886fa3562b76da9b1f1789dd5
Semsaid <semsaid@gmail.com>
2023-09-08 12:18:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - this is the function puts to print C program
- *
- * Return; always 0
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
