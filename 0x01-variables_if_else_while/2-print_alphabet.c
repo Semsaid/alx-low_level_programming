2d60e33bf598d05516a79f307298cdf60bd54b88
Semsaid <semsaid@gmail.com>
2023-09-10 13:27:33 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main is function of alphabet game
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
