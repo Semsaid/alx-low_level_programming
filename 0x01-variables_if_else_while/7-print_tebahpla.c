6915506e70816eaa53c5fad45875572b62f56e01
Semsaid <semsaid@gmail.com>
2023-09-10 15:45:47 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main is a function defining Smile is the mirror
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z' ; x >= 'a' ; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
