14aaa75c746a62ecb0513e0ebbf870d42f7dabf4
Semsaid <semsaid@gmail.com>
2023-09-10 14:34:11 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main is the function to Write a program that prints the alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char x;
+
+for (x = 'a' ; x <= 'z' ; x++)
+if (x != 'q' && x != 'e')
+putchar(x);
+putchar('\n');
+return (0);
+}
