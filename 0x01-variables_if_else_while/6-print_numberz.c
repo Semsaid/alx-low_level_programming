0d27075b1f4b6f025c0398138397e1e44bc3334a
Semsaid <semsaid@gmail.com>
2023-09-10 15:24:32 -0700

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main is the function of Numberz
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		putchar(x + 0);
+	putchar('\n');
+	return (0);
+}
