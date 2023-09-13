4b21f166a11d1cfb849e439edbec1f4194fde397
Semsaid <semsaid@gmail.com>
2023-09-13 11:57:36 -0700

    on GOD

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet in lower case
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		for (y = 'a' ; y <= 'z' ; y++)
+			_putchar(y);
+	}
+	_putchar('\n');
+
+	return (0);
+}
