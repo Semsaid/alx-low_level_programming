8531ef5543820f44db37a116230c2c898b7fadba
Semsaid <semsaid@gmail.com>
2023-09-13 13:00:57 -0700

    on GOD

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - function that prints the alphabet, in lowercase
+ * print_alphabet - function that prints the alphabet, in lowercase
  *
  * Return: void;
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char x;
 
