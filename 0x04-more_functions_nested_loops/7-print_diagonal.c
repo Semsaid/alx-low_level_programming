77a207f76c82cfe80b460d760212488c4052f7ec
Semsaid <semsaid@gmail.com>
2023-09-15 18:33:32 -0700

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,18 @@
 
 void print_diagonal(int n)
 {
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
-		int x, y;
+if (n <= 0)
+_putchar('\n');
+else
+{
+int x, y;
 
-		for (x = 0 ; x < n ; x++)
-		{
-			for (y = 0 ; y <= x ; y++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
+for (x = 0 ; x < n ; x++)
+{
+for (y = 0 ; y <= x ; y++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
 }
