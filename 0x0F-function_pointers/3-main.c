6eea7141064bc7f482a22942175636e71f96a30d
Semsaid <semsaid@gmail.com>
2023-10-13 08:22:19 -0700

    single code

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -31,13 +31,13 @@ printf("Error\n");
 exit(99);
 }
 
-if ((*op == '/' && num2 == 0) ||
-(*op == '%' && num2 == 0))
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
 }
 
 printf("%d\n", get_op_func(op)(num1, num2));
+
 return (0);
 }
